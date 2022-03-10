025c16c115178f08cc041bfb26a4592b9daaba3c
anuj <anujgupta596@gmail.com>
2022-03-10 17:35:19 +0530

    component created

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddContectComponent } from './add-contect/add-contect.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { DeleteContactComponent } from './delete-contact/delete-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddContectComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ListContactComponent,
+    DeleteContactComponent,
+    ViewContactComponent
+  ],
   imports: [
     CommonModule
   ]
